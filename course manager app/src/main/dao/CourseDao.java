2aa0bb32890ebd17459d278e433a634683318fe1
vanshhiwase12 <112774712+vanshhiwase12@users.noreply.github.com>
2023-05-13 16:48:23 +0530

    Delete course manager app directory

--- course manager app/src/main/dao/CourseDao.java
+++
@@ -1,7 +0,0 @@
-package com.course_manager.dao;
-
-import com.course_manager.entities.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseDao extends JpaRepository<Course, Long> {
-}
